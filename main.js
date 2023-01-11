e159ab1ea7651fe21fea7b225faca53444fb0d83
Great Ben <bchanan.boss@gmail.com>
2023-01-11 06:45:57 +0100

    this is my commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const btn = document.querySelector('.btn');
+const number = document.querySelector('.copy')
+const input = document.querySelector('#input')
+
+btn.addEventListener('click', generate)
+
+const math = () => {
+	Math.floor(Math.random(1000 * 10))
+}
+
+function generate() {
+	const math = [1234, 1235, 8902, 1234, 1653, 1777, 1888, 9282, 0000, 8277];
+	const generated = math[Math.floor(Math.random() * 10)];
+	localStorage.setItem('gen-input', generated);
+	number.innerHTML = `<p>${generated}</p>`
+	
+	setTimeout(() => {
+		location.href = './welcome.html'
+	}, 2000)
+}
+
