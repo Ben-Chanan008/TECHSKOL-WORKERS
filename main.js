31b2cf63b1f7d3755a770ee6976cb69b911c5889
Great Ben <bchanan.boss@gmail.com>
2023-01-11 23:19:20 +0100

    this is my commit

--- main.js
+++ main.js
@@ -11,12 +11,11 @@ const math = () => {
 function generate() {
 	const math = [1234, 1235, 8902, 1234, 1653, 1777, 1888, 9282, 0000, 8277];
 	const generated = math[Math.floor(Math.random() * 10)];
-	localStorage.setItem('gen-input', generated);
+	localStorage.setItem('gen-input', generated.toString());
 	number.innerHTML = `<p>${generated}</p>`
 	
 	setTimeout(() => {
-		if (localStorage.getItem('gen-input'))
-			location.href = './welcome.html'
+		location.href = './welcome.html'
 	}, 2000)
 }
 
